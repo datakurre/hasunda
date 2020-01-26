593061a3e688205fa9444187cd562d534ba6db02
Asko Soukka <asko.soukka@iki.fi>
2020-01-26 20:32:41 +0200

    Cleanup

--- other-examples/tomcat-camunda-bpm-ee/docker-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export $(cat camunda-enterprise-login.env | xargs)
-docker build --build-arg EE_USERNAME="${EE_USERNAME}" --build-arg EE_PASSWORD="${EE_PASSWORD}" .
--- other-examples/wildfly+postgres/postgres-camunda-bpm-ee/camunda-enterprise-license.sql
+++
@@ -1,6 +0,0 @@
-insert into ACT_GE_PROPERTY
-(NAME_, VALUE_, REV_)
-values
-('camunda-license-key',
-'your;license;key',
-1)
